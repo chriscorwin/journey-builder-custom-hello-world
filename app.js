6a9ce754513071b6cf2f1c231409990b83ab57b0
Chris Corwin <CCorwin@exacttarget.com>
2014-04-17 16:04:15 -0400

    (master) WIP

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.post('/ixn/triggers/hello-world/', trigger.edit);
 // Abstract Event Handler
 app.post('/fireEvent/:type', function(req, res) {
 	var data = req.body;
-	var triggerIdFromAppExtensionInAppCenter = '__insert_your_trigger_key_here__';
+	var triggerIdFromAppExtensionInAppCenter = 'jb-hello-world-trigger-ccorwin';
 	var JB_EVENT_API = 'https://www.exacttargetapis.com/interaction-experimental/v1/events';
 	var reqOpts = {};
 
