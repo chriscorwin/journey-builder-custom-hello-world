bc5591bac1766c3b368703677f33aea122a3019b
Chris Corwin <CCorwin@exacttarget.com>
2014-04-17 15:06:49 -0400

    (master) updated app settings

--- app.js
+++ app.js
@@ -2,78 +2,80 @@
 // Module Dependencies
 // -------------------
 //require('newrelic');
-var express     = require('express');
-var http        = require('http');
-var JWT         = require('./lib/jwtDecoder');
-var path        = require('path');
-var request     = require('request');
-var routes      = require('./routes');
-var activity    = require('./routes/activity');
-var trigger     = require('./routes/trigger');
+var express = require('express');
+var http = require('http');
+var JWT = require('./lib/jwtDecoder');
+var path = require('path');
+var request = require('request');
+var routes = require('./routes');
+var activity = require('./routes/activity');
+var trigger = require('./routes/trigger');
 
 var app = express();
 
 // Register configs for the environments where the app functions
 // , these can be stored in a separate file using a module like config
 var APIKeys = {
-    appId           : '__insert_your_app_id__',
-    clientId        : '__insert_your_app_client_id__',
-    clientSecret    : '__insert_your_app_client_secret__',
-    appSignature    : '__insert_your_app_signature__',
-    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
+	appId: '0b6f895a-8b15-4d66-8bca-0004aa5a73ff',
+	clientId: '7vqd3g55v9twq9ftf7r8hzf4',
+	clientSecret: 'jpRHfbDPm6F4HP6jWpza77Ps',
+	appSignature: '23lslazst0pm22ghfarlpp2naejg4qhghcm5qgk2qmehx1ws4u3mat5cjckidfzlqfbmqqleon4tzozmmmzklxacmevh51g3gtgqm5sfptxxgxn1haeutfoszdzzy0wxcnubaazwmmzszfe0vdodttqrp4hm1cwn12wwaomc5rbyfv45mzcmeahxguaqvf0pt04izwmllu3guo2kbvdy02zxw024szb331o5dhg04atbhz0dkjd23lfmx33cijb',
+	authUrl: 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
 };
 
 // Simple custom middleware
-function tokenFromJWT( req, res, next ) {
-    // Setup the signature for decoding the JWT
-    var jwt = new JWT({appSignature: APIKeys.appSignature});
-    
-    // Object representing the data in the JWT
-    var jwtData = jwt.decode( req );
+function tokenFromJWT(req, res, next) {
+	// Setup the signature for decoding the JWT
+	var jwt = new JWT({
+		appSignature: APIKeys.appSignature
+	});
 
-    // Bolt the data we need to make this call onto the session.
-    // Since the UI for this app is only used as a management console,
-    // we can get away with this. Otherwise, you should use a
-    // persistent storage system and manage tokens properly with
-    // node-fuel
-    req.session.token = jwtData.token;
-    next();
+	// Object representing the data in the JWT
+	var jwtData = jwt.decode(req);
+
+	// Bolt the data we need to make this call onto the session.
+	// Since the UI for this app is only used as a management console,
+	// we can get away with this. Otherwise, you should use a
+	// persistent storage system and manage tokens properly with
+	// node-fuel
+	req.session.token = jwtData.token;
+	next();
 }
 
 /**
 THIS IS A WORKAROUND FOR A KNOWN BUG, DO NOT USE THIS CODE IN PRODUCTION
 **/
-function workaround( req, res, next ) {
-	if( 'POST' !== req.method ) {
+function workaround(req, res, next) {
+	if ('POST' !== req.method) {
 		next();
 	}
 
-	if( '/login' === req.url || '/fireEvent/helloWorld' === req.url ) {
+	if ('/login' === req.url || '/fireEvent/helloWorld' === req.url) {
 		next();
 	}
-	
-	if( 
-		'/ixn/activities/hello-world/save/'		=== req.url ||
-		'/ixn/activities/hello-world/execute/'	=== req.url ||
-		'/ixn/activities/hello-world/publish/'	=== req.url ||
+
+	if (
+		'/ixn/activities/hello-world/save/' === req.url ||
+		'/ixn/activities/hello-world/execute/' === req.url ||
+		'/ixn/activities/hello-world/publish/' === req.url ||
 		'/ixn/activities/hello-world/validate/' === req.url
-	){
+	) {
 		var buf = '';
-		req.on('data', function(chunk){
+		req.on('data', function(chunk) {
 			buf += chunk;
 		});
 
-		req.on('end', function(){
-			try{
+		req.on('end', function() {
+			try {
 				var faultyJSON = /"", *}/;
 				// Cleanup Jira: JB-5249
-				if( buf.match( faultyJSON ) ) {
-					req.body = buf.replace( faultyJSON, '""}' );
+				if (buf.match(faultyJSON)) {
+					req.body = buf.replace(faultyJSON, '""}');
 				}
 				next();
-			} catch( err ){
-				console.error( 'ERROR: ', err );
-				next( err );
+			} catch (err) {
+				console.error('ERROR: ', err);
+				next(err);
 			}
 			next();
 		});
@@ -84,7 +86,9 @@ function workaround( req, res, next ) {
 app.use(express.cookieParser());
 
 // TODO: MaxAge for cookie based on token exp?
-app.use(express.cookieSession({secret: "HelloWorld-CookieSecret"}));
+app.use(express.cookieSession({
+	secret: "HelloWorld-CookieSecret"
+}));
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
@@ -101,75 +105,79 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Express in Development Mode
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 // HubExchange Routes
-app.get('/', routes.index );
-app.post('/login', tokenFromJWT, routes.login );
-app.post('/logout', routes.logout );
+app.get('/', routes.index);
+app.post('/login', tokenFromJWT, routes.login);
+app.post('/logout', routes.logout);
 
 // Custom Hello World Activity Routes
-app.post('/ixn/activities/hello-world/save/', activity.save );
-app.post('/ixn/activities/hello-world/validate/', activity.validate );
-app.post('/ixn/activities/hello-world/publish/', activity.publish );
-app.post('/ixn/activities/hello-world/execute/', activity.execute );
+app.post('/ixn/activities/hello-world/save/', activity.save);
+app.post('/ixn/activities/hello-world/validate/', activity.validate);
+app.post('/ixn/activities/hello-world/publish/', activity.publish);
+app.post('/ixn/activities/hello-world/execute/', activity.execute);
 
 // Custom Hello World Trigger Route
-app.post('/ixn/triggers/hello-world/', trigger.edit );
+app.post('/ixn/triggers/hello-world/', trigger.edit);
 
 // Abstract Event Handler
-app.post('/fireEvent/:type', function( req, res ) {
-    var data = req.body;
-    var triggerIdFromAppExtensionInAppCenter = '__insert_your_trigger_key_here__';
-    var JB_EVENT_API = 'https://www.exacttargetapis.com/interaction-experimental/v1/events';
-    var reqOpts = {};
+app.post('/fireEvent/:type', function(req, res) {
+	var data = req.body;
+	var triggerIdFromAppExtensionInAppCenter = '__insert_your_trigger_key_here__';
+	var JB_EVENT_API = 'https://www.exacttargetapis.com/interaction-experimental/v1/events';
+	var reqOpts = {};
 
-    if( 'helloWorld' !== req.params.type ) {
-        res.send( 400, 'Unknown route param: "' + req.params.type +'"' );
-    } else {
-        // Hydrate the request
-        reqOpts = {
-            url: JB_EVENT_API,
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer ' + req.session.token
-            },
-            body: JSON.stringify({
-                ContactKey: data.alternativeEmail,
-                EventDefinitionKey: triggerIdFromAppExtensionInAppCenter,
-                Data: data
-            })
-        };
+	if ('helloWorld' !== req.params.type) {
+		res.send(400, 'Unknown route param: "' + req.params.type + '"');
+	} else {
+		// Hydrate the request
+		reqOpts = {
+			url: JB_EVENT_API,
+			method: 'POST',
+			headers: {
+				'Authorization': 'Bearer ' + req.session.token
+			},
+			body: JSON.stringify({
+				ContactKey: data.alternativeEmail,
+				EventDefinitionKey: triggerIdFromAppExtensionInAppCenter,
+				Data: data
+			})
+		};
 
-        request( reqOpts, function( error, response, body ) {
-            if( error ) {
-                console.error( 'ERROR: ', error );
-                res.send( response, 400, error );
-            } else {
-                res.send( body, 200, response);
-            }
-        }.bind( this ) );
-    }
+		request(reqOpts, function(error, response, body) {
+			if (error) {
+				console.error('ERROR: ', error);
+				res.send(response, 400, error);
+			} else {
+				res.send(body, 200, response);
+			}
+		}.bind(this));
+	}
 });
 
-app.get('/clearList', function( req, res ) {
+app.get('/clearList', function(req, res) {
 	// The client makes this request to get the data
 	activity.logExecuteData = [];
-	res.send( 200 );
+	res.send(200);
 });
 
 
 // Used to populate events which have reached the activity in the interaction we created
-app.get('/getActivityData', function( req, res ) {
+app.get('/getActivityData', function(req, res) {
 	// The client makes this request to get the data
-	if( !activity.logExecuteData.length ) {
-		res.send( 200, {data: null} );
+	if (!activity.logExecuteData.length) {
+		res.send(200, {
+			data: null
+		});
 	} else {
-		res.send( 200, {data: activity.logExecuteData} );
+		res.send(200, {
+			data: activity.logExecuteData
+		});
 	}
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
