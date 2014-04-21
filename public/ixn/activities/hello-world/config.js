81a823e996c1c3ea00e80db60d0c9260f12df52c
Chris Corwin <CCorwin@exacttarget.com>
2014-04-21 13:03:46 -0400

    (master) modified config.js for the custom activity

--- public/ixn/activities/hello-world/config.js
+++ public/ixn/activities/hello-world/config.js
@@ -1,57 +1,57 @@
-define([], function(){      
-    return {
-        "icon": "images/jb-icon.jpg",
-        "iconSmall": "images/jb-icon.jpg", 
-        "key": "__insert_your_custom_activity_key_here__",
-        "partnerApiObjectTypeId": "IXN.CustomActivity.REST",
-        "lang": {
-            "en-US": {        
-                "name": "Hello World",
-                "description": "Activity simply posts the data into an array for display on the App's home page."
-            }
-        },
-        "category": "messaging",
-        "version": "1.0",
-        "apiVersion": "1.0",
-       "execute": {
-            "uri": "https://__insert_your_web_server_endpoint_here__/ixn/activities/hello-world/execute/",
+define([], function() {
+	return {
+		"icon": "images/jb-icon.jpg",
+		"iconSmall": "images/jb-icon.jpg",
+		"key": "jb-hello-world-activity-ccorwin",
+		"partnerApiObjectTypeId": "IXN.CustomActivity.REST",
+		"lang": {
+			"en-US": {
+				"name": "Hello World (ccorwin)",
+				"description": "Activity simply posts the data into an array for display on the App's home page."
+			}
+		},
+		"category": "messaging",
+		"version": "1.0",
+		"apiVersion": "1.0",
+		"execute": {
+			"uri": "https://quiet-crag-9311.herokuapp.com/ixn/activities/hello-world/execute/",
 			"inArguments": [],
 			"outArguments": [],
-            "verb": "POST",
-			"body": "",
-            "format": "json",
-            "useJwt": false,
-            "timeout": 3000
-		},
-        "save": {
-            "uri": "https://__insert_your_web_server_endpoint_here__/ixn/activities/hello-world/save/",
 			"verb": "POST",
 			"body": "",
-            "format": "json",
-            "useJwt": false,
-            "timeout": 3000
-        },
-        "publish": {
-            "uri": "https://__insert_your_web_server_endpoint_here__/ixn/activities/hello-world/publish/",
-            "verb": "POST",
+			"format": "json",
+			"useJwt": false,
+			"timeout": 3000
+		},
+		"save": {
+			"uri": "https://quiet-crag-9311.herokuapp.com/ixn/activities/hello-world/save/",
+			"verb": "POST",
 			"body": "",
-            "format": "json",
-            "useJwt": false,
-            "timeout": 3000
-        },
-        "validate": {
-            "uri": "https://__insert_your_web_server_endpoint_here__/ixn/activities/hello-world/validate/",
-            "verb": "POST",
+			"format": "json",
+			"useJwt": false,
+			"timeout": 3000
+		},
+		"publish": {
+			"uri": "https://quiet-crag-9311.herokuapp.com/ixn/activities/hello-world/publish/",
+			"verb": "POST",
 			"body": "",
-            "format": "json",
-            "useJwt": false,
-            "timeout": 3000
-        },
+			"format": "json",
+			"useJwt": false,
+			"timeout": 3000
+		},
+		"validate": {
+			"uri": "https://quiet-crag-9311.herokuapp.com/ixn/activities/hello-world/validate/",
+			"verb": "POST",
+			"body": "",
+			"format": "json",
+			"useJwt": false,
+			"timeout": 3000
+		},
 
-        "edit": {
-            "uri": "https://__insert_your_web_server_endpoint_here__/ixn/activities/hello-world/",
-            "height": 400,
-            "width": 500
-        }
-};
+		"edit": {
+			"uri": "https://quiet-crag-9311.herokuapp.com/ixn/activities/hello-world/",
+			"height": 400,
+			"width": 500
+		}
+	};
 });
