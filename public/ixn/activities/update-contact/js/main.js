9f771f4bc2f470e490f3912272434da2680063bc
Chris Corwin <CCorwin@exacttarget.com>
2014-05-08 13:36:31 -0400

    (master) WIP

--- public/ixn/activities/update-contact/js/main.js
+++ public/ixn/activities/update-contact/js/main.js
@@ -1,3 +1,31 @@
+requirejs.config({
+	paths: {
+		vendor: '../vendor',
+		postmonger: 'vendor/postmonger'
+	},
+	shim: {
+		'vendor/jquery.min': {
+			exports: '$'
+		},
+		'UpdateContact': {
+			deps: ['vendor/jquery.min', 'vendor/postmonger']
+		}
+	}
+});
+
+requirejs(['vendor/jquery.min', 'UpdateContact'], function($, UpdateContact) {
+	//console.log( 'REQUIRE LOADED' );
+});
+
+requirejs.onError = function(err) {
+	//console.log( "REQUIRE ERROR: ", err );
+	if (err.requireType === 'timeout') {
+		console.log('modules: ' + err.requireModules);
+	}
+
+	throw err;
+};
+
 debug.setLevel(5);
 
 var editGroupTemplateSource = $('#edit-group-template').html();
